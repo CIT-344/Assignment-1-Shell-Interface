b5ee7c3e2b0a1a0d5a919723746808b79e8f9ccc
Bailey Miller <bmiller@susquehannasoftware.com>
2018-09-04 11:43:44 -0400

    Lots of new commands

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
 using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Assignment_1_Shell_Interface
 {
@@ -23,7 +26,7 @@ namespace Assignment_1_Shell_Interface
 
             var CommandLine = Console.ReadLine().Split(' ');
             var Command = CommandLine[0].Trim();
-            var Paramaters = CommandLine.Length >= 2 ? CommandLine[1].Trim(): "";
+            var Paramaters = CommandLine.Length >= 2 ? BuildParams(CommandLine.Skip(1)): "";
 
             switch (Command)
             { 
@@ -45,9 +48,27 @@ namespace Assignment_1_Shell_Interface
                 case "clear":
                     Console.Clear();
                     break;
+
                 case "cls":
                     Console.Clear();
                     break;
+
+                case "date":
+                    SpawnShellProcess("date");
+                    break;
+
+                case "cd":
+                    ChangeWorkingDirectory(Paramaters);
+                    break;
+
+                case "mkdir":
+                    SpawnShellProcess(String.Concat(Command, " ", Paramaters));
+                    break;
+
+                case "rm":
+                    SpawnShellProcess(String.Concat(Command, " ", Paramaters));
+                    break;
+
                 default:
                     Console.WriteLine("Unsupported command");
                     break;
@@ -57,6 +78,54 @@ namespace Assignment_1_Shell_Interface
 
             // Enter Loop
             EnterCommandLoop();
+        }
+
+        private static string BuildParams(IEnumerable<string> enumerable)
+        {
+            var builder = new StringBuilder();
+
+            foreach (var spacer in enumerable)
+            {
+                builder.Append(spacer + " ");
+            }
+
+            return builder.ToString().Trim(); 
+        }
+
+        private static void ChangeWorkingDirectory(string paramaters)
+        {
+            // Change the Environment.CurrentDirectory value
+            var paths = paramaters.Split('/');
+            var dirInfo = new DirectoryInfo(Environment.CurrentDirectory);
+            var tempDirInfo = dirInfo;
+
+            foreach (var pathItem in paths)
+            {
+                if (pathItem == "..")
+                {
+                    // Go up to the current parents dir
+                    tempDirInfo = tempDirInfo.Parent;
+                }
+                else
+                {
+                    // Some other named dir
+                    // First check if that dir exists in the current dir structure
+                    if (tempDirInfo.GetDirectories().Where(x => x.Name == pathItem).Count() != 0)
+                    {
+                        // There is a dir with a matching name
+                        tempDirInfo = tempDirInfo.GetDirectories().Where(x => x.Name == pathItem).FirstOrDefault();
+                    }
+                    else
+                    {
+                        // Throw some kind of error about pathing
+                        
+                    }
+                }
+            }
+
+
+            Environment.CurrentDirectory = tempDirInfo != null ? tempDirInfo.FullName : dirInfo.FullName;
+
         }
 
         private static void Exit()
